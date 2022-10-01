f0ec1aafe9613af03ce098aee3c2ca656681f998
Tommaso Gagliardi <tommasogagliardi.pv@gmail.com>
2022-10-01 18:21:10 -0400

    Add files via upload

---
+++ Six_Nations.sql
@@ -0,0 +1,49 @@
+create database six_nations;
+
+create table england
+(player varchar(50),
+position varchar(20),
+date_birth date,
+caps smallint,
+year_ch int
+);
+
+create table france
+(player varchar(50),
+position varchar(20),
+date_birth date,
+caps int,
+year int
+);
+
+create table ireland
+(player varchar(50),
+position varchar(20),
+date_birth date,
+caps int,
+year int
+);
+
+create table italy
+(player varchar(50),
+position varchar(20),
+date_birth date,
+caps int,
+year int
+);
+
+create table scotland
+(player varchar(50),
+position varchar(20),
+date_birth date,
+caps int,
+year int
+);
+
+create table wales
+(player varchar(50),
+position varchar(20),
+age date,
+caps int,
+year int
+);
